48f3c65aaf653819c48bb36b6e2e5698d8bf4033
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-17 19:26:20 +0700

    s

--- plugins/supabase.js
+++ plugins/supabase.js
@@ -1,7 +1,10 @@
-// supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://txjdflhtrowtagbzdtwe.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4amRmbGh0cm93dGFnYnpkdHdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgwMDAxMDksImV4cCI6MjAzMzU3NjEwOX0.OVpcAq-6IaMPucaLE7wK_CxVUe5m1HJEr-hlW5K30zU'
-export const supabase = createClient(supabaseUrl, supabaseKey)
-export { supabase }
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default ({ app }, inject) => {
+  inject('supabase', supabase)
+}
