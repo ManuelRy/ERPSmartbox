18a0c7a9cd23b28ded365db65545dc3793af99e9
ManuelRy <121281145+ManuelRy@users.noreply.github.com>
2024-06-19 17:13:31 +0700

    s

--- components/Profile/Card.vue
+++ components/Profile/Card.vue
@@ -15,7 +15,7 @@ const user = useSupabaseUser();
 </script>
 <template>
 <section class="">
-     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+     <div class="flex flex-col items-center min-h-screen justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <img v-if="user" class="w-24 h-24 mb-3 rounded-full shadow-lg" :src="user.user_metadata.avatar_url || '/img/default_avatar.jpg'" alt="User image"/>
         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Name: {{ user.user_metadata.full_name }}</h5>
         <span class="text-sm text-gray-500 dark:text-gray-400">Email: {{ user.email }}</span>
